c8a4d1108a599e0745659ff756c997441b62d258
Amit Varma <44401673+Amit927@users.noreply.github.com>
2018-11-05 19:32:40 +0530

    Update SearchPageTest.java

--- src/test/java/com/jdTouch/testcases/SearchPageTest.java
+++ src/test/java/com/jdTouch/testcases/SearchPageTest.java
@@ -2,7 +2,9 @@ package com.jdTouch.testcases;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.jdTouch.base.BaseClass;
@@ -13,64 +15,64 @@ public class SearchPageTest extends BaseClass{
 	
 	 HomePage homepage;
 	 SearchPage searchpage;
+	 static boolean status = false;
 	
 	public SearchPageTest() {
 		super();
 	}
 	
-	@BeforeClass
+@BeforeClass
 	public void  setup() throws InterruptedException {
 		initialization();
 		homepage = new HomePage();
 	}
 	
-	
-	
-	
-	@Test (priority=2)
-	public void testSearchBarRedirection() {
+@BeforeMethod
+public void testSearchBarRedirection() {
 		 searchpage = homepage.checkSearchBarRedirection();
-	//	Assert.assertTrue(searchpage);
-		System.out.println("Pass");
+	
 	}
 	
-	@Test (priority=3)
-	public void testSearchPage() throws InterruptedException {
-		boolean status1 = searchpage.validateSearchPage();
-		Assert.assertTrue(status1);
-		System.out.println("Pass");
-		Thread.sleep(2000);
+	@Test (priority=1)
+	public void testWhatField() {
+		status = searchpage.validateWhatfield();
+		Assert.assertTrue(status);
+	}
+	
+	@Test (priority=2)
+	public void testWherefield() {
+		status = searchpage.validateWherefield();
+		Assert.assertTrue(status);
 	}
 	
 	@Test (priority=4)
-	public void testSearchCategoryAutosuggest() throws InterruptedException {
+	public void testSearchCategoryAutosuggest()  {
 		boolean status2 = searchpage.checkAutosuggest("categorydata");
-		Assert.assertTrue(status2);
-		System.out.println("Pass");
+		Assert.assertTrue(status);
 	}
 	
 	@Test (priority=5)
-	public void testSearchCompanyAutosuggest() throws InterruptedException {
+	public void testSearchCompanyAutosuggest()  {
 		boolean status2 = searchpage.checkAutosuggest("companydata");
-		Assert.assertTrue(status2);
-		System.out.println("Pass");
+		Assert.assertTrue(status);
 	}
 	
 	@Test (priority=6)
-	public void testSearchGroupAutosuggest() throws InterruptedException {
+	public void testSearchGroupAutosuggest() {
 		boolean status2 = searchpage.checkAutosuggest("groupdata");
-		Assert.assertTrue(status2);
-		System.out.println("Pass");
+		Assert.assertTrue(status);
 	}
 	
 	@Test (priority=7)
-	public void testSearchProductAutosuggest() throws InterruptedException {
+	public void testSearchProductAutosuggest() {
 		boolean status2 = searchpage.checkAutosuggest("productdata");
-		Assert.assertTrue(status2);
-		System.out.println("Pass");
+		Assert.assertTrue(status);
 	}
 	
-	
+@AfterMethod
+public void navigateToHomePage() {
+	searchpage.checkSearchpageBack();
+}
 	
 	
 @AfterClass
